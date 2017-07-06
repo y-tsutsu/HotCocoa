6473318dabd89183b2cdbfcdc709dffa1704aa3f
tsutsu <tsutsu0928@gmail.com>
2017-07-06 23:19:42 +0900

    Visual Studio 2017にコンバート

--- HotCocoa/MainActivity.cs
+++ HotCocoa/MainActivity.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Linq;
-using System.Text;
 using Android.App;
-using Android.Content;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
